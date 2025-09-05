b519a7edf96499a8d00108d2da1efc702b104535
Ambuj Shrivastav <ambujshri4@gmail.com>
2025-09-05 18:09:20 +0530

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const { getTasks, createTask, updateTask, deleteTask, shareTask } = require('../controllers/taskController');
 const auth = require('../middleware/authMiddleware');
-
 const router = express.Router();
-
 router.use(auth);
-
 router.get('/', getTasks);
 router.post('/', createTask);
 router.put('/:id', updateTask);
